c9927247fa97f6d7955d28f96357a474456ca786
pauquinc <91104452+pauquinc@users.noreply.github.com>
2024-02-19 20:42:31 -0500

    Nice up to this point

--- Public/main.js
+++ Public/main.js
@@ -1,31 +1,35 @@
 console.log("Test");
 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '861c82f77fmsh570dccbb039c37fp15eaf5jsn85e33d56f0cd',
-		'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '861c82f77fmsh570dccbb039c37fp15eaf5jsn85e33d56f0cd',
+        'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
+    }
 };
 
 // Async & wait
-async function getInfo(word){
-try{
-    const foobar = await fetch( `https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${word}`, options);
-    
-    const infoFromServer = await foobar.json();
-    console.log( infoFromServer.list[0].definition);
- 
-    const content = document.querySelector("#wordDef");
-    content.innerHTML = 
-    `<p>${infoFromServer.list[0].word}</p>
+async function getInfo(word) {
+    try {
+        const foobar = await fetch(`https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${word}`, options);
+
+        const infoFromServer = await foobar.json();
+        console.log(infoFromServer.list[0].definition);
+
+        const content = document.querySelector("#wordDef");
+        content.innerHTML =
+            `
+    <b>Word</b>
+    <p>${infoFromServer.list[0].word}</p>
+    <b>Definition</b>
     <p>${infoFromServer.list[0].definition}</p>
-    <p>${infoFromServer.list[0].definition}</p>
+    <b>Example</b>
+    <p>${infoFromServer.list[0].example}</p>
     `;
 
-} catch( error ) {
-    console.warn( `Nope: ${error}` );
+    } catch (error) {
+        console.warn(`Nope: ${error}`);
+    }
 }
-}
- 
+
 getInfo('clapter');
